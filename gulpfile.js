814d52f22c582aec913ae009624b6cfa2699cf36
artshpakov <artshpakov@yandex.ru>
2015-07-18 12:55:11 +0300

    Enter Bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+  bower  = require('main-bower-files'),
   concat = require('gulp-concat'),
   coffee = require('gulp-coffee');
 
@@ -10,9 +11,16 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest('./static/js/'));
 });
 
+gulp.task('deps', function() {
+  gulp.src(bower())
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('./static/js/'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./src/javascript/**/*.coffee', ['coffee']);
 });
 
-gulp.task('build', ['coffee']);
+
+gulp.task('build', ['deps', 'coffee']);
 gulp.task('default', ['build', 'watch']);
